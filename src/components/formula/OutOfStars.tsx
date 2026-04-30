ef2e496203afd463c33a159cee5a9f3f42cbe08f
WriFe Build <wrife.education@gmail.com>
2026-04-30 09:32:44 +0100

    fix(stars): remove unused useNavigate import in OutOfStars

--- src/components/formula/OutOfStars.tsx
+++ src/components/formula/OutOfStars.tsx
@@ -6,7 +6,6 @@
  */
 
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 interface OutOfStarsProps {
   onReview: () => void   // navigate to review / dashboard
